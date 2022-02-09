406f4badcbb2cb8b54f0089fa838163cfe604936
Joe Abousselam <joe.abousselam@gmail.com>
2022-02-09 15:41:27 +0000

    add docblocks to BuilderApi facade

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes to `redsnapper/builder-api` will be documented in this file.
 
+## 1.0.1 - 2022-02-09
+- add docblocks to BuilderApi facade
+
 ## 1.0.0 - 2022-01-27
 
 - initial release
--- src/Facades/BuilderApi.php
+++ src/Facades/BuilderApi.php
@@ -3,7 +3,13 @@
 namespace RedSnapper\Builder\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use RedSnapper\Builder\BuilderResponse;
+use RedSnapper\Builder\PendingRequest;
 
+/**
+ * @method static PendingRequest new()
+ * @method static BuilderResponse get()
+ */
 class BuilderApi extends Facade
 {
     protected static function getFacadeAccessor(): string
